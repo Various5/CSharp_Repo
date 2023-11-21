1cb848283b7c8f87cc99bad622121b66a10f72bf
Various5 <44600128+Various5@users.noreply.github.com>
2023-11-21 20:55:26 +0100

    Add files via upload

--- DragDropItem.cs
+++ DragDropItem.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-//test
+
 public class DragDropItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform rectTransform;
